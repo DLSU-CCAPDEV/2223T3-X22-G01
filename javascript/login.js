2c040f9e3b98e839bdeefdff34013b9174e653ce
Kate Rojo <106814132+kit-kate15@users.noreply.github.com>
2023-06-23 02:25:17 +0800

    Update login.js

--- javascript/login.js
+++ javascript/login.js
@@ -1,4 +1,5 @@
 const form_Open_Btn = document.querySelector("#form-open"),
+form_Post_Open_Btn = document.querySelector("#open_form_post"),
 home = document.querySelector(".home"),
 form_Content = document.querySelector(".form_content"),
 form_Close_Btn = document.querySelector(".form_close"),
@@ -6,8 +7,18 @@ signup_Btn = document.querySelector("#sign-up"),
 login_Btn = document.querySelector("#login"),
 pw_Show_Hide = document.querySelectorAll(".pw_hide");
 
-form_Open_Btn.addEventListener("click", () => home.classList.add("show"));
-form_Close_Btn.addEventListener("click", () => home.classList.remove("show"));
+form_Open_Btn.addEventListener("click", () => toggleFormVisibility(true));
+form_Post_Open_Btn.addEventListener("click", () => toggleFormVisibility(true));
+
+form_Close_Btn.addEventListener("click", () => toggleFormVisibility(false));
+
+function toggleFormVisibility(show) {
+  if (show) {
+    home.classList.add("show");
+  } else {
+    home.classList.remove("show");
+  }
+}
 
 pw_Show_Hide.forEach((icon) => {
   icon.addEventListener("click", () => {
@@ -30,3 +41,4 @@ login_Btn.addEventListener("click", (e) => {
   e.preventDefault();
   form_Content.classList.remove("active");
 });
+
