f0c608f8481001eade0cb57bc281708bf0c97692
katrombs <105538604+katrombs@users.noreply.github.com>
2023-06-23 13:49:00 +0800

    Add files via upload

--- javascript/post-page.js
+++ javascript/post-page.js
@@ -15,23 +15,127 @@ $(document).ready( function(){
     }
 
     // edit post
-    // content('#post-content',"whatever");
+    $('#post-edit').off('click').on('click', function(){
+        var postContent = document.getElementById("post-content-main");
+        var postBody = document.getElementById("post-body");
+        postContent.setAttribute("contenteditable", "true");
+        
+        var editFooter = document.createElement('div');
+        var editConfirm = document.createElement('button');
+        editConfirm.setAttribute("contenteditable", "false");
+        $(editFooter).addClass('edit-footer');
+        $(editConfirm).addClass('edit-button');
+        $(editConfirm).text("Edit");
+        editFooter.appendChild(editConfirm);
+        postBody.appendChild(editFooter);
+
+    });
+
+    $('#edit-button').off('click').on('click', function(){
+        $(postContent).setAttribute("contenteditable", "false");
+        $(editFooter).remove();
+    });
     
     // delete post
+    $('#post-delete').click( function(){
+        var postPage = document.getElementById("post-page-content-1");
+        var mainPost = document.getElementById("main-post-1");
+        var commentBox = document.getElementById("comment-box-1");
+        var comments = document.getElementById("comment-section-1");
 
+        var noticeBox = document.createElement('div');
+        $(noticeBox).addClass('notice-box');
+        $(noticeBox).text("This post has been deleted.");
+        postPage.appendChild(noticeBox);
+        
+        $(mainPost).remove();
+        $(commentBox).remove();
+        $(comments).remove();
+    });
     
     // vote update
 
 
     // add comment
     $('.comment-button').click( function(){
-        var comment = $('.comment-box').val();
-        var div_element = document.createElement('div');
+        var div_comment;
+        var div_left, div_right;
+        var div_pfp;
+        var div_vote;
+        var div_commenter;
+        var div_comment_content;
+        commentSection = document.getElementById("comment-section-1"),
+        comment = document.createDocumentFragment();
 
-        $(div_element).text(comment);
-        $(div_element).addClass('comment-content');
-        //<p class='tweet_text'> tweet </p>
-        $('#comment-content').append(div_element);
+        //div for comment
+        div_comment = document.createElement('div');
+
+        $(div_comment).addClass('comment');
+        comment.appendChild(div_comment);
+
+        //div left
+        div_left = document.createElement('div');
+
+        $(div_left).addClass('left-comment');
+        div_comment.appendChild(div_left);
+
+        //div right
+        div_right = document.createElement('div');
+
+        $(div_right).addClass('right-comment');
+        div_comment.appendChild(div_right);
+
+        //pfp
+        div_pfp = document.createElement('div');
+        // var img = document.createElement('img');
+        // img.src = "../images/eve_square.jpg"
+        // div_pfp.appendChild(img);
+
+        $(div_pfp).addClass('pfp-comment');
+        div_left.appendChild(div_pfp);
+
+        //vote
+        div_vote = document.createElement('div');
+        
+        var upvote = document.createElement('ion-button');
+        // $(upvote).text('<ion-icon slot="icon-only" name="caret-up-outline"></ion-icon>');
+        // $(upvote).addClass('upvote');
+        upvote.innerHTML = '<ion-icon slot="icon-only" name="caret-up-outline" class="upvote"></ion-icon>';
+        div_vote.appendChild(upvote);
+
+        var numvote = document.createElement('div');
+        $(numvote).text("0");
+        div_vote.appendChild(numvote);
+
+        var downvote = document.createElement('ion-button');
+        // $(downvote).text('<ion-icon slot="icon-only" name="caret-down-outline"></ion-icon>');
+        // $(downvote).addClass('downvote');
+        downvote.innerHTML = '<ion-icon slot="icon-only" name="caret-down-outline" class="downvote"></ion-icon>';
+        div_vote.appendChild(downvote);
+
+        $(div_vote).addClass('vote-comment');
+        div_left.appendChild(div_vote);
+
+        //commenter
+        div_commenter = document.createElement('div');
+        var span = document.createElement('span');
+
+        $(span).addClass('commenter-clickable');
+        $(span).text("@eveiscool");
+        div_commenter.appendChild(span);
+
+        $(div_commenter).addClass('commenter');
+        div_right.appendChild(div_commenter);
+
+        //comment content
+        var commentContent = $('.comment-box').val();
+        div_comment_content = document.createElement('div');
+
+        $(div_comment_content).text(commentContent);
+        $(div_comment_content).addClass('comment-content');
+        div_right.appendChild(div_comment_content);
+
+        commentSection.appendChild(comment);
     });
 
     // edit comment
@@ -44,7 +148,3 @@ $(document).ready( function(){
 function postSettings(){
     document.getElementById("setoptions").classList.toggle("show");
 }
-
-// function content(divSelector, value) {
-//     document.querySelector(divSelector).innerHTML = value;
-// }
