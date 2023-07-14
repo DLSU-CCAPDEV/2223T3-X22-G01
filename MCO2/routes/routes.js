047bf75ed011d12026851b35a596346933a4677a
katrombs <105538604+katrombs@users.noreply.github.com>
2023-07-14 22:54:57 +0800

    Add files via upload

---
+++ MCO2/routes/routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const controller = require('../controllers/controller.js');
+
+const app = express();
+
+app.get('/favicon.ico', controller.getFavicon);
+
+app.get('/', controller.getIndex);
+
+module.exports = app;
