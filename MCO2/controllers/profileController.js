d5230ed1b7bf2dd9aded9d009089281632204638
katrombs <105538604+katrombs@users.noreply.github.com>
2023-07-16 22:28:36 +0800

    Add files via upload
    incomplete functions

---
+++ MCO2/controllers/profileController.js
@@ -0,0 +1,10 @@
+/*
+
+// import module `database` from `../models/db.js`
+const db = require('../models/db.js');
+
+// import module `User` from `../models/UserModel.js`
+const User = require('../models/UserModel.js');
+
+*/
+
