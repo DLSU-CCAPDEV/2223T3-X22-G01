4c403058ee466687eb6d0586baa71b5f000e2995
katrombs <105538604+katrombs@users.noreply.github.com>
2023-07-16 22:39:10 +0800

    to change

--- MCO2/controllers/profileController.js
+++ MCO2/controllers/profileController.js
@@ -8,3 +8,47 @@ const User = require('../models/UserModel.js');
 
 */
 
+const profileController = {
+    getProfile: async function (req, res) {
+
+        // query where `idNum` is equal to URL parameter `idNum`
+        var query = {idNum: req.params.idNum};
+
+        // fields to be returned
+        var projection = 'fName lName idNum';
+
+        /*
+            calls the function findOne()
+            defined in the `database` object in `../models/db.js`
+            this function searches the collection `users`
+            based on the value set in object `query`
+            the third parameter is a string containing fields to be returned
+        */
+        var result = await db.findOne(User, query, projection);
+
+        /*
+            if the user exists in the database
+            render the profile page with their details
+        */
+        if(result != null) {
+            var details = {
+                fName: result.fName,
+                lName: result.lName,
+                idNum: result.idNum
+            };
+
+            // render `../views/profile.hbs`
+            res.render('profile', details);
+        }
+
+        /*
+            if the user does not exist in the database
+            render the error page
+        */
+        else {
+            res.render('error');
+        }
+    }
+}
+
+module.exports = profileController;
