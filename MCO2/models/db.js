ab77e8fd8d544d71b6e30946623b96ca6182ac88
katrombs <105538604+katrombs@users.noreply.github.com>
2023-07-17 19:38:12 +0800

    updated mongodb uri

--- MCO2/models/db.js
+++ MCO2/models/db.js
@@ -3,8 +3,8 @@ import User from "../models/UserModel.js";
 import Post from "../models/PostModel.js";
 import Comment from "../models/CommentModel.js";
 
-// Connect to MONGODB_URL, which also includes the database name
-const url = process.env.MONGODB_URL;
+// Connect to MONGODB_URI, which also includes the database name
+const url = process.env.MONGODB_URI;
 
 export default {
   /*
