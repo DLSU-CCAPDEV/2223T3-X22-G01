7695c4924df06229d87d09e8d1a3bb8a25112454
katrombs <105538604+katrombs@users.noreply.github.com>
2023-07-19 05:49:10 +0800

    image to db (incomplete)

---
+++ MCO2/models/ImageModel.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var ImageSchema = new mongoose.Schema({
+    name: String,
+    img:
+    {
+        data: Buffer,
+        contentType: String
+    }
+});
+ 
+module.exports = mongoose.model('Image', ImageSchema);
