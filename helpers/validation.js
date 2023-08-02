58ced19fa9f8f1e5aabebf7bd246ffe9d6829537
katrombs <105538604+katrombs@users.noreply.github.com>
2023-08-02 11:06:23 +0800

    validation for signup

--- helpers/validation.js
+++ helpers/validation.js
@@ -1,7 +1,16 @@
 const { check } = require('express-validator');
 
 const validation = {
-    
+    signupValidation: function () {
+        var validation = [
+            check('displayName', 'Display name should not be empty.').notEmpty(),
+            check('username', 'Username should not be empty.').notEmpty(),
+            check('password', 'Password should contain at least 8 characters.').isLength({min: 8}),
+            check('password2', 'Password does not match!').equals('password2', 'password')
+        ];
+
+        return validation;
+    }
 
 };
 
